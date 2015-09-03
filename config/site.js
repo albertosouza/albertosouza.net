56135efe85fd98ce0990736af0526e170f0e2f3a
albertosouza <alberto.souza.99@gmail.com>
2015-09-03 11:49:09 -0300

    add site logo

--- config/site.js
+++ config/site.js
@@ -1,6 +1,12 @@
+var path = require('path');
 module.exports = {
   appName: 'Site do Alberto Souza',
   subtitle: 'Programando o futuro ...',
+
+  // default favicon, change in your project config/local.js
+  favicon: path.resolve(__dirname, '..', 'files/public/favicon.png'),
+  // logo public url path
+  appLogo: '/public/project/logo.jpg',
 
   site: {
     homeBg :'/public/project/home-bg.jpg'
