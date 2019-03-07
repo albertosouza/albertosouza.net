6c08d811104398edc786a4c225ea9ac94a3e1d9a
albertosouza <alberto.souza.99@gmail.com>
2019-03-07 11:11:06 -0300

    remove uneed theme configs

--- config/themes.js
+++ config/themes.js
@@ -1,9 +1,1 @@
-module.exports.themes = {
-  enabled: [
-    {
-      name: 'we-theme-blog-startbootstrap-clean',
-      themeFolder: process.cwd()+'/server/themes/we-theme-blog-startbootstrap-clean'
-    }
-  ],
-  app: 'we-theme-blog-startbootstrap-clean'
-};
+module.exports.themes = {};
